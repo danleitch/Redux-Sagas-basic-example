deecfb27222285774299b1553426c9bbec60fea5
Daniel Leitch <dndleitch@gmail.com>
2022-03-28 13:24:36 +0200

    adding redux

---
+++ Redux/sagas.js
@@ -0,0 +1,22 @@
+import { put, takeEvery } from 'redux-saga/effects';
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export function* helloSaga() {
+  console.log('Hello Sagas!');
+}
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+  // yield delay(1000);
+  yield put({ type: 'INCREMENT' });
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+// export function* watchIncrementAsync() {
+//   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+// }
+
+export default function* rootSaga() {
+  yield all([helloSaga(), watchIncrementAsync()]);
+}
